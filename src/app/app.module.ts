d83b4848e658edc35e43b65ec5a85c82f35cb91a
JosueSantos49 <conceicaojosue@yahoo.com.br>
2024-07-18 11:43:20 -0300

    Incluindo exibição de mensagens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,24 @@ import { FormsModule } from '@angular/forms';
 import { HeroesDetailComponent } from './heroes-detail/heroes-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MessagesComponent } from './messages/messages.component';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroesDetailComponent],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroesDetailComponent,
+    MessagesComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,24 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hereos = HEROES;
+  hereos: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    this.heroService.getHeroes().subscribe((hereos) => (this.hereos = hereos));
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HereosComponent: herói selecionado id=${hero.id}`);
   }
 }
