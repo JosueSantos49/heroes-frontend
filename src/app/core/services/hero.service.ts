fdb82ca5d14c05ded14a4e35b33f6bb18e1eaa8b
JosueSantos49 <conceicaojosue@yahoo.com.br>
2024-08-08 12:09:20 -0300

    Incluindo estrutura para simular um backend

--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -1,24 +1,46 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from '../models/hero.model';
-import { Observable, of } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = 'api/heroes';
 
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
+
+  //GET /heroes
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: busca heroes');
-    return heroes;
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(tap((heroes) => this.log(`buscou ${heroes.length} hero(es)`)));
+
+    /*const heroes = of(HEROES);
+    this.log('busca heroes');
+    return heroes;*/
   }
 
+  // GET /heroes/id
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find((hero) => hero.id === id)!;
-    this.messageService.add(`HeroService: busca heroes id=${id}`);
-    return of(hero);
+    return this.http
+      .get<Hero>(`${this.heroesUrl}/${id}`)
+      .pipe(
+        tap((hero) => this.log(`busca heroes id=${id} e nome=${hero.name}`))
+      );
+
+    /*const hero = HEROES.find((hero) => hero.id === id)!;
+    this.log(`busca heroes id=${id}`);
+    return of(hero);*/
+  }
+
+  private log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://toh-api.vercel.app/api',
+};
